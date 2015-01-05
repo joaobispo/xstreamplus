6a07e15bfe38e284ac328535c57cd5414bf1c8df
joaobispo <joaobispo@gmail.com>
2015-01-05 00:54:34 +0000

    - Added project; - Added XStreamUtils, with read, write and toString methods;

---
+++ xstreamplus/src/main/java/org/specs/xstreamplus/XStreamUtils.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2015 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package org.specs.xstreamplus;
+
+import java.io.File;
+
+import org.specs.library.IoUtils;
+
+import com.thoughtworks.xstream.XStream;
+
+public class XStreamUtils {
+
+    /**
+     * Helper method for XStream 'fromXML'. This version is unsafe, if the output class is not the same as the file
+     * being read, throws an exception (ClassCastException?).
+     * 
+     * @param file
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T read(File file) {
+	return (T) new XStream().fromXML(file);
+    }
+
+    /**
+     * Converts an object to XML and then writes it to a file.
+     * 
+     * @param file
+     * @return
+     */
+    public static void write(Object contents, File file) {
+	IoUtils.write(toString(contents), file);
+    }
+
+    /**
+     * Helper method for XStream 'toXML'.
+     * 
+     * @param contents
+     * @return
+     */
+    public static String toString(Object contents) {
+	return new XStream().toXML(contents);
+    }
+}
---
+++ xstreamplus/src/test/java/org/gradle/PersonTest.java
@@ -0,0 +1,12 @@
+package org.gradle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonTest {
+    @Test
+    public void canConstructAPersonWithAName() {
+	assertTrue(true);
+    }
+}
